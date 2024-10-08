04294241cca3543051473ad33b1c2d301991d406
Rachel Wallace <Wallace-R20@ulster.ac.uk>
2024-10-08 10:53:35 +0100

    Updated code with access modifiers

---
+++ Library/Book.cs
@@ -0,0 +1,29 @@
+﻿namespace Library
+{
+    class Book
+    {
+        string Title;
+        string Author;   //store 3 pieces of data about the book
+        string ISBN;
+        int NoOfPages;
+
+        public Book(string bookTitle, string bookAuthor, string bookISBN, int bookNoOfPages)
+        {
+            Title = bookTitle;
+            Author = bookAuthor;
+            ISBN = bookISBN;
+            NoOfPages = bookNoOfPages;
+        }
+
+        public void DisplayInfo()   //method called DisplayInfo inside class Book
+        {
+            Console.WriteLine("Book Information");
+            Console.WriteLine("----------------------");
+            Console.WriteLine($"Book title: {Title}");
+            Console.WriteLine($"Book author: {Author}");  //refer to variables
+            Console.WriteLine($"Book ISBN: {ISBN}");
+            Console.WriteLine($"Book Pages: {NoOfPages}");
+            Console.WriteLine();
+        }
+    }
+}
--- Library/Program.cs
+++ Library/Program.cs
@@ -1,29 +1,7 @@
-﻿class Book
-{
-    string Title;
-    string Author;   //store 3 pieces of data about the book
-    string ISBN;
-    int NoOfPages;
+﻿using Library;
 
-    public Book(string bookTitle, string bookAuthor, string bookISBN, int bookNoOfPages)
-    {
-        Title = bookTitle;
-        Author = bookAuthor;
-        ISBN = bookISBN;
-        NoOfPages = bookNoOfPages;
-    }
-
-    void DisplayInfo()   //method called DisplayInfo inside class Book
-    {
-        Console.WriteLine("Book Information");
-        Console.WriteLine("----------------------");
-        Console.WriteLine($"Book title: {Title}");
-        Console.WriteLine($"Book author: {Author}");  //refer to variables
-        Console.WriteLine($"Book ISBN: {ISBN}");
-        Console.WriteLine($"Book Pages: {NoOfPages}");
-        Console.WriteLine();
-    }
-
+    class Program
+{ 
 
     static void Main(string[] args)
     {
@@ -39,3 +17,4 @@
 
     }
 }
+
